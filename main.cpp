d6af97a493f2a9d7ef41769338abeedbdca9e1b9
No9 <antonwhalley@vodafone.ie>
2013-09-09 00:49:12 +0100

    very basic put

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string>
+#include <iostream>
+#include <vector>
 #include <uv.h>
 #include <unistd.h>
 #include "leveldb/db.h"
 
 uv_loop_t *loop;
+leveldb::DB* db;
 
 uv_buf_t alloc_buffer(uv_handle_t *handle, size_t suggested_size) {
     return uv_buf_init((char*) malloc(suggested_size), suggested_size);
@@ -30,9 +33,32 @@ void luvdbie_read(uv_stream_t *client, ssize_t nread, uv_buf_t buf) {
     uv_write_t *req = (uv_write_t *) malloc(sizeof(uv_write_t));
     req->data = (void*) buf.base;
     buf.len = nread;
-     
-   // if (s.ok()) s = db->Put(leveldb::WriteOptions(), key2, value);
-       uv_write(req, client, &buf, 1, luvdbie_write);
+    char messagetype = buf.base[0];
+    unsigned int keylength = 0;
+    unsigned int valuelength = 0;
+    unsigned int curPos = 2;
+    //Ignore message type and first !
+    std::string keyl;
+    //std::string vall;
+    for(int i = 2; i < 32; i++){ 
+        if(buf.base[i] == '!') {
+           if(keylength == 0) {
+              keyl = std::string(buf.base + curPos, buf.base + i);
+              keylength = atoi(keyl.c_str());
+              curPos = i + 1;  
+              
+              std::cout << "key: " << keyl << "$" << std::endl; 
+              break;        
+           }
+        }
+    }
+
+   //Test for empty key and empty value....
+   leveldb::Status s = db->Put(leveldb::WriteOptions(), 
+                               std::string(buf.base + curPos, keylength), 
+                               std::string(buf.base + curPos + keylength, buf.len));
+    
+   uv_write(req, client, &buf, 1, luvdbie_write);
 }
 
 void on_new_connection(uv_stream_t *server, int status) {
@@ -62,7 +88,7 @@ int main(int argc, char *argv[]) {
               break;
         }       
     }
-    leveldb::DB* db;
+    
     leveldb::Options options;
     options.create_if_missing = true;
     leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,9 @@ var net = require('net')
 var connection = net.connect(8085, "localhost", function() { // connect to the server we made
     console.log("client connected");
     setInterval(function () { // every 1000ms,
-       connection.write("er-mah-gerd");// write to the connection
+       var j = {key:"1", value:"test"}
+       var msg = "p!" + j.key.length + "!" + j.key + j.value; 
+       connection.write(msg);// write to the connection
     }, 1000);//the 1000 is the delay in between the anon function calls
 })
 .on('data', function(data) {
